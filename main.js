978570daf1616ea70893539ff04b0f914fbf3085
tukey42 <tukey@t-online.de>
2023-08-01 11:28:26 +0200

    Zone and programs first version tested

--- main.js
+++ main.js
@@ -7,9 +7,44 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
+const { Zones, Zone } = require('./lib/zone.js');
+const { Programs, Program } = require('./lib/program.js');
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
+/*
+Zone:
+
+Nummer
+Name
+STATE Device
+ON_TIME Device
+Standardmäßige Bewässerungszeit
+
+Programm:
+Name
+Zonenliste
+Liste Bewässerungszeit
+Parallel/Sequentiell
+enable/disable
+program läuft
+
+
+Schedule:
+wochentag Uhrzeit, ....
+
+Zonenliste:
+program Nummer
+Zonennummer
+Order
+mit/nach vorhergegangenen
+Bewässerungszeit
+Wetterabhängig
+
+Wetterbeeinflussung:
+Temperatur
+Wind
+Regen
+Vorhersage
+*/
 
 class IrrigationControl extends utils.Adapter {
 
@@ -23,9 +58,10 @@ class IrrigationControl extends utils.Adapter {
         });
         this.on('ready', this.onReady.bind(this));
         this.on('stateChange', this.onStateChange.bind(this));
+        this.on('unload', this.onUnload.bind(this));
         // this.on('objectChange', this.onObjectChange.bind(this));
         // this.on('message', this.onMessage.bind(this));
-        this.on('unload', this.onUnload.bind(this));
+
     }
 
     /**
@@ -37,55 +73,25 @@ class IrrigationControl extends utils.Adapter {
         // Reset the connection indicator during startup
         this.setState('info.connection', false, true);
 
+        this.zones = Zones.getInstance(this);
+        await this.zones.initialize();
+        this.log.info(`Get Zone list: ${JSON.stringify(this.zones.getzonelist())}`);
+
+        this.progs = Programs.getInstance(this);
+        await this.progs.initialize();
+
         // The adapters config (in the instance object everything under the attribute "native") is accessible via
         // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
+        //this.log.info('config option1: ' + this.config.option1);
+        //this.log.info('config option2: ' + this.config.option2);
 
-        /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
-        await this.setObjectNotExistsAsync('testVariable', {
-            type: 'state',
-            common: {
-                name: 'testVariable',
-                type: 'boolean',
-                role: 'indicator',
-                read: true,
-                write: true,
-            },
-            native: {},
-        });
+        // Zones.create_zone(this, new Zone('Rasen hinten links', 'statedev', 'ontimedev', 60, true));
+        // Zones.create_zone(this, new Zone('Rasen hinten rechts', 'statedev', 'ontimedev', 60, true));
+        //Programs.create_program(this, new Program('Rasenflächen', ['Rasen hinten links', ['Rasen hinten rechts', 100]], false, true));
+        //Programs.create_program(this, new Program('Rasenflächen', [{ 'name': 'Rasen hinten links', 'duration' : 0  }, 
+        //                                                           { 'name': 'Rasen hinten rechts', 'duration': 100} ]));
+        this.setState('info.connection', true, true);
 
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
-
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
-
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
-
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
-
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
     }
 
     /**
@@ -94,34 +100,12 @@ class IrrigationControl extends utils.Adapter {
      */
     onUnload(callback) {
         try {
-            // Here you must clear all timeouts or intervals that may still be active
-            // clearTimeout(timeout1);
-            // clearTimeout(timeout2);
-            // ...
-            // clearInterval(interval1);
-
+            if (this.zones) this.zones.stopAll();
             callback();
         } catch (e) {
             callback();
         }
     }
-
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  * @param {string} id
-    //  * @param {ioBroker.Object | null | undefined} obj
-    //  */
-    // onObjectChange(id, obj) {
-    //     if (obj) {
-    //         // The object was changed
-    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    //     } else {
-    //         // The object was deleted
-    //         this.log.info(`object ${id} deleted`);
-    //     }
-    // }
 
     /**
      * Is called if a subscribed state changes
@@ -136,25 +120,28 @@ class IrrigationControl extends utils.Adapter {
             // The state was deleted
             this.log.info(`state ${id} deleted`);
         }
+        if (id.startsWith(this.namespace)) {
+            const comp = id.split('.');
+            const i = id.substring(this.namespace.length+1);
+            if (comp[2] == 'zones' && this.zones) {
+                this.zones.stateChange(i, state);
+                this.progs.zoneChange(i, state);
+            }
+            if (comp[2] == 'programs' && this.progs) this.progs.stateChange(i, state);
+        } else {
+            this.zones.stateChange(id, state);
+        }
     }
 
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    //     if (typeof obj === 'object' && obj.message) {
-    //         if (obj.command === 'send') {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info('send command');
-
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    //         }
-    //     }
-    // }
+    onObjectChange(id, obj) {
+        if (obj) {
+            this.log.info(`object ${id} changed:  (${JSON.stringify(obj)})`);
+            // Objekt geändert
+        } else {
+            this.log.info(`object ${id} deleted`);
+            // Objekt gelöscht
+        }
+    }
 
 }
 
